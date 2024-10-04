a5232b8d62d0b6e4ceccab7619b77d42ae67f3ee
JPCrawford <55292933+JohnnyPgood@users.noreply.github.com>
2024-10-04 12:52:59 -0500

    Add logo click to refresh background image

--- wwwroot/script.js
+++ wwwroot/script.js
@@ -33,16 +33,49 @@ function apiSearch() {
         });
 }
 
-$(document).ready(function () {
+/* Background Image Lists */
+const images = ['antelope-canyon.jpg', 'aurora.jpg', 'canal-city.jpg', 'colosseum.jpg',
+    'forest.jpg', 'grand-canyon.jpg', 'hotair-balloons.jpg', 'ice-mountain.jpg',
+    'misty-peak.jpg', 'night-sky.jpg', 'river-boats.jpg', 'river-rocks.jpg',
+    'rock-sculpture.jpg', 'rocky-waves.jpg', 'snow-ghost.jpg', 'spinal-tunnel.jpg',
+    'sunset-hills.jpg', 'vanagon.jpg'];
+let unusedImages = [...images];
 
-    /* Random background on load */
-    const images = ['antelope-canyon.jpg', 'aurora.jpg', 'canal-city.jpg', 'colosseum.jpg',
-        'forest.jpg', 'grand-canyon.jpg', 'hotair-balloons.jpg', 'ice-mountain.jpg',
-        'misty-peak.jpg', 'night-sky.jpg', 'river-boats.jpg', 'river-rocks.jpg',
-        'rock-sculpture.jpg', 'rocky-waves.jpg', 'snow-ghost.jpg', 'spinal-tunnel.jpg',
-        'sunset-hills.jpg', 'vanagon.jpg'];
-    const randomBkg = images[Math.floor(Math.random() * images.length)];
+/* Set random background that hasn't been used */
+function newBackground() {
+    if (unusedImages.length === 0) {
+        unusedImages = [...images];
+    }
+    const randomIndex = Math.floor(Math.random() * unusedImages.length);
+    const randomBkg = unusedImages.splice(randomIndex, 1)[0];
     $('body').css('background-image', `url('media/bkg/${randomBkg}')`);
+}
+
+/* Gopilot query */
+function gopilotQuery(queryVal) {
+    const encodedQuery = encodeURIComponent(queryVal);
+    const gopilotUrl = `https://lmgtfy.click/?q=${encodedQuery}`;
+    window.location.href = gopilotUrl;
+}
+
+/* Gopilot dialog */
+function gopilotAlert() {
+    $('#gopilotDialog').dialog({
+        modal: true,
+        open: function() {
+            $('#gpQuery').focus();
+        }
+    });
+}
+
+$(document).ready(function () {
+    /* Random background on load */
+    newBackground();
+
+    /* New background on logo click */
+    $('#bingle').on('click', function() {
+        newBackground();
+    });
 
     /* Search query with button click */
     $('#searchButton').on('click', function() {
@@ -56,29 +89,12 @@ $(document).ready(function () {
         }
     });
 
-    /* Gopilot query */
-    function gopilotQuery(queryVal) {
-        const encodedQuery = encodeURIComponent(queryVal);
-        const gopilotUrl = `https://lmgtfy.click/?q=${encodedQuery}`;
-        window.location.href = gopilotUrl;
-    }
-
     /* Gopilot query with Enter */
     $('#gpQuery').on('keypress', function(e) {
         if (e.which === 13 && $('#gpQuery').val() !== '') {
             gopilotQuery($('#gpQuery').val());
         }
     });
-
-    /* Gopilot dialog */
-    function gopilotAlert() {
-        $('#gopilotDialog').dialog({
-            modal: true,
-            open: function() {
-                $('#gpQuery').focus();
-            }
-        });
-    }
 
     /* Gopilot Button Press */
     $('#gopilotButton').on('click', function () {
